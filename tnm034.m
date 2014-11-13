ed6a75ee97d05c50a0eaf03a04b4601e71d08683
Hannes Ingelhag <hanin602@student.liu.se>
2014-11-13 17:56:49 +0100

    Detektion funkar på svartvita bilder

--- tnm034.m
+++ tnm034.m
@@ -1,12 +1,5 @@
 function [strout] = tnm034(img)
 
-
-%   binary1 = flipud(img([9:16] ,1)).';
-% %   binary2 = flipud(img([17:24] ,1)).';
-%   binary3 = flipud(img([25:32] ,1)).';
-%   
-%   binary4 = fliplr([0 1 1 0 0 1 1 1]);
-
 detectFiducial(img);
 
 binMat = zeros(183, 8);
@@ -40,20 +33,5 @@ end
 
 char(charMat)
 
-% char(bi2de(binary1))
-% char(bi2de(binary2))
-% char(bi2de(binary3))
-% char(bi2de(binary4))
-
-
-% 
-% for i = 9:33
-%     sout(1, i-8) = img(i);
-% end
-
-
-%bi2de(sout)
-
-%img = imresize(img, [41 41]);
 
 
