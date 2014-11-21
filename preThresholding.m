155d78cade7ea7f0c37537824312f717a5919c1b
Hannes Ingelhag <hanin602@student.liu.se>
2014-11-21 15:53:34 +0100

    Annan thres

--- preThresholding.m
+++ preThresholding.m
@@ -1,10 +1,15 @@
 function[newPic] = preThresholding(img)
 close all;
-imshow(img)
-img = im2double(img);
-fun = @(x) max(x(:));
-B = nlfilter(img,[20 20],fun);
-newPic = img./B;
+% imshow(img)
+% img = im2double(img);
+% fun = @(x) max(x(:));
+% B = nlfilter(img,[20 20],fun);
+% newPic = img./B;
+
+maxPic = max(img(:));
+minPic = min(img(:));
+
+newPic = img > (maxPic+minPic)/2;
 
 figure;
 imshow(newPic)
